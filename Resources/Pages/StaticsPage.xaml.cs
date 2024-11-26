e7b5f70cb461826f8993e42af038f27361431049
shkraiev <shkraev07@gmail.com>
2024-11-26 15:29:59 +0100

    The first basic version of StaticsPage (without functionality) has been released

--- Resources/Pages/StaticsPage.xaml.cs
+++ Resources/Pages/StaticsPage.xaml.cs
@@ -1,11 +1,39 @@
 namespace Habbit.Resources.Pages;
+using System.ComponentModel;
+using System.Xml.Linq;
 
-public partial class StaticsPage : ContentPage
+
+[QueryProperty(nameof(AvatarUrl), "AvatarUrl")]
+[QueryProperty(nameof(Name), "Name")]
+public partial class StaticsPage : ContentPage, INotifyPropertyChanged
 {
-	public StaticsPage()
+
+    private string avatarUrl;
+    public string AvatarUrl
+    {
+        get => avatarUrl;
+        set
+        {
+            avatarUrl = value;
+            OnPropertyChanged(nameof(AvatarUrl));
+        }
+    }
+
+    private string name;
+    public string Name
+    {
+        get => name;
+        set
+        {
+            name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+    public StaticsPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this; // Прив'язка контексту
+    }
 
 
     
