ec9633c8d52612e75cbcfc8ce5b457806c678830
Zionert <marczionert@gmail.com>
2024-11-30 20:25:47 +0100

    Design of Add Page

--- Resources/Pages/AddPage.xaml.cs
+++ Resources/Pages/AddPage.xaml.cs
@@ -7,5 +7,79 @@ public partial class AddPage : ContentPage
 		InitializeComponent();
 	}
 
-   
+    // Habit RadioButton CheckedChanged Handler
+    private void OnHabitRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value) // When selected
+        {
+            HabitRadioButton.BackgroundColor = Color.FromArgb("#8EC1F3");
+            HabitRadioButton.TextColor = Colors.White;
+
+            GoalRadioButton.BackgroundColor = Color.FromArgb("#B0B0B0");
+            GoalRadioButton.TextColor = Colors.Black;
+        }
+    }
+
+    // Goal RadioButton CheckedChanged Handler
+    private void OnGoalRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value) // When selected
+        {
+            GoalRadioButton.BackgroundColor = Color.FromArgb("#8EC1F3");
+            GoalRadioButton.TextColor = Colors.White;
+
+            HabitRadioButton.BackgroundColor = Color.FromArgb("#B0B0B0");
+            HabitRadioButton.TextColor = Colors.Black;
+        }
+    }
+
+
+    // RadioButton CheckedChanged Handlers
+    private void OnStrengthRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value) // When selected
+        {
+            StrengthRadioButton.BackgroundColor = Color.FromArgb("#8EC1F3");
+            StrengthRadioButton.TextColor = Colors.White;
+        }
+        else // When deselected
+        {
+            StrengthRadioButton.BackgroundColor = Color.FromArgb("#B0B0B0");
+            StrengthRadioButton.TextColor = Colors.Black;
+        }
+    }
+
+    private void OnIntelligenceRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value) // When selected
+        {
+            IntelligenceRadioButton.BackgroundColor = Color.FromArgb("#8EC1F3");
+            IntelligenceRadioButton.TextColor = Colors.White;
+        }
+        else // When deselected
+        {
+            IntelligenceRadioButton.BackgroundColor = Color.FromArgb("#B0B0B0");
+            IntelligenceRadioButton.TextColor = Colors.Black;
+        }
+    }
+
+    private void OnCharismaRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value) // When selected
+        {
+            CharismaRadioButton.BackgroundColor = Color.FromArgb("#8EC1F3");
+            CharismaRadioButton.TextColor = Colors.White;
+        }
+        else // When deselected
+        {
+            CharismaRadioButton.BackgroundColor = Color.FromArgb("#B0B0B0");
+            CharismaRadioButton.TextColor = Colors.Black;
+        }
+    }
+
+    private void AddTask(object sender, EventArgs e)
+    {
+
+    }
+
 }
